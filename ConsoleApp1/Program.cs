f000c4152990840a0a5400ece05d10c41e2abcc0
Pn0 <superumesyu@gmail.com>
2025-12-02 16:57:05 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int x = 10;
+int y = 5;
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
