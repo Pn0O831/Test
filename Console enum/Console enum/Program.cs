ca0f9c1c36ca79293726345c8b857742ed99c5ec
Pn0O831 <superumesyu@gmail.com>
2025-12-05 12:05:37 +0900

    Add files via upload

---
+++ Console enum/Console enum/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Sample
+{
+    enum Weekday
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+    }
+
+    class Sample
+    {
+        static void Main()
+        {
+            foreach (Weekday day in Enum.GetValues(typeof(Weekday)))
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
+
+
+
+
